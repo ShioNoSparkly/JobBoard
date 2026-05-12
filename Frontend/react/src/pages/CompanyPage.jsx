7df0283d9909bb1e46ee0152d003aa98cf14f388
Cristina Buffone <crimlb87@gmail.com>
2026-05-12 19:48:48 +0200

    promemoria di ciò che si deve fare su CompanyPage e UserPage

--- Frontend/react/src/pages/CompanyPage.jsx
+++ Frontend/react/src/pages/CompanyPage.jsx
@@ -1,7 +1,14 @@
 function CompanyPage(prams) {
     return (
         <>
+        <h1>CompanyPage</h1>
+        <p>Qui deve esserci la dashboard con gli annunci filtrati per id della singola azienda</p>
+        <p>La possibilità di rimuovere o aggiungere annunci</p>
+        <p>collegare l'invio delle candidature dell'utente all'azienda di destinazione</p>
+        <p>La possibilità di accettare o rifiutare le candidature degli utenti</p>
+         <button>Esci</button>
         </>
+       
 
     )
 
--- Frontend/react/src/pages/UserPage.jsx
+++ Frontend/react/src/pages/UserPage.jsx
@@ -1,6 +1,10 @@
 function UserPage(prams) {
     return (
         <>
+        <h1>UserPage</h1>
+        <p>Qui devono esserci gli annunci aggiunti ai preferiti dall'utente con possibilità di rimuoverle</p>
+        <p>La possibilità di vedere lo stato della sua candidatura e la possibilità di rimuoverla</p>
+        <button>Esci</button>
         </>
 
     )
