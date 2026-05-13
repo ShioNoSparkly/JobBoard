407ecde6f613163cc0dba5679edbbc6bcbe3280f
Cristina Buffone <crimlb87@gmail.com>
2026-05-13 14:17:42 +0200

    fix testo dinamico per Comaìpany e Candidate

--- Frontend/react/src/pages/Jobspage.jsx
+++ Frontend/react/src/pages/Jobspage.jsx
@@ -8,6 +8,12 @@ function Jobspage() {
   //     .then(res => setJobs(res.data));
   // }, []);
 
+
+//esempio  utente loggato
+  const user = {
+    role: "user", // "candidate" oppure "company"
+  };
+
   const jobs = [
     {
       id: 1,
@@ -40,16 +46,36 @@ function Jobspage() {
     },
   ];
 
+
+  const heroContent = {
+    candidate: {
+      title: "Trova il lavoro dei tuoi sogni!",
+      subtitle: "Scopri le migliori offerte disponibili per la tua carriera.",
+    },
+
+    company: {
+      title: "Scopri i migliori talenti!",
+      subtitle: "Pubblica offerte e seleziona i candidati ideali per la tua azienda.",
+    },
+  };
+
+  const currentHero =
+    user.role === "company"
+      ? heroContent.company
+      : heroContent.candidate;
+
+
+
   return (
     <>
       <section className="hero-section d-flex align-items-center py-5 bg-img-2">
         <div className= "overlay z-0"></div>
           <div className="container py-5 position-relative">
             <h1 className="display-3 fw-bold mb-3 text-dark z-1">
-              Trova il lavoro perfetto
+               {currentHero.title}
             </h1>
-            <p className="lead text-dark fw-bold">
-              Scopri le migliori offerte disponibili.
+            <p className="lead text-dark fw-bold fs-4">
+               {currentHero.subtitle}
             </p>
           </div>
       </section>
