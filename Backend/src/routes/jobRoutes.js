52857f4650eb023bb3fe9b2709787d0f95fca1e5
SchiAgo <agostinoschiattarella@gmail.com>
2026-05-13 08:50:35 +0200

    prima sistemazione

--- Backend/src/routes/jobRoutes.js
+++ Backend/src/routes/jobRoutes.js
@@ -1,18 +1,31 @@
 
 
-// routes/jobRoutes.js
+
 const express = require('express');
 const router = express.Router();
+
+const { body, param, query } = require("express-validator");
+const validate = require("../middleware/validate");
 const jobController = require('../controllers/jobController');
 
-const { autenticato, soloAzienda } = require("../middleware/auth");
+const {
+    autenticato,
+    soloAzienda,
+    soloCandidato,
+} = require("../middleware/auth");
+
+const regolaId = [
+    param("id")
+        .isInt({ min: 1 })
+        .withMessage("L'id deve essere un numero intero positivo"),
+];
 
 // Sostituiamo il "mock" di prima con i middleware reali
 
 console.log("Ciao dal router job")
 
 // TEST: Creazione annuncio senza middleware
-// uso un ID esistente nel tuo DB (es. 1)
+// uso un ID esistente nel DB (es. 1)
 router.post('/test-create', (req, res, next) => {
 
     console.log("Dentro la rotta test-create")
@@ -21,9 +34,15 @@ router.post('/test-create', (req, res, next) => {
     next();
 }, jobController.createJob);
 
+router.get("/",
+    autenticato,
+    jobController.getAllJobs
+);
+
 router.post("/",
     autenticato,
     soloAzienda,
+    validate,
     jobController.createJob
 );
 
